eee4abaedbddaa31d2d5372c3647bddfcc695116
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2019-06-11 16:42:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,14 +6,7 @@ import os
 
 
 #subprocess.check_call("C:\\Users\\TyouK\\Documents\\GitHub\\build\\test\\src\\Release\\XXXTest.exe")
-try:
-    ret = subprocess.check_call("C:\\workspace\\build\\test\\src\\Release\\XXXTest.exe")
-except subprocess.CalledProcessError as e:
-    print(e.returncode)
-    print(e.cmd)
-    print(e.output)
-except:
-    print("error")
+ret = subprocess.check_call("C:\\workspace\\build\\test\\src\\Release\\XXXTest.exe")
 
 
 aaaa
