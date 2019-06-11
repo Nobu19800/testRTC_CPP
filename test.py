4f574aa0939f68e8667464ba5b4084769e492f76
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2019-06-11 16:43:06 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,3 @@ import os
 
 #subprocess.check_call("C:\\Users\\TyouK\\Documents\\GitHub\\build\\test\\src\\Release\\XXXTest.exe")
 ret = subprocess.check_call("C:\\workspace\\build\\test\\src\\Release\\XXXTest.exe")
-
-
-aaaa
