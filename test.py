a07394bd23d1c1947bf9d6a7558f7e638f425836
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2019-06-11 16:21:40 +0900

    update

--- test.py
+++ test.py
@@ -4,8 +4,13 @@
 import subprocess
 import os
 
-print(os.environ["PATH"])
-
 
 #subprocess.check_call("C:\\Users\\TyouK\\Documents\\GitHub\\build\\test\\src\\Release\\XXXTest.exe")
-subprocess.check_call("C:\\workspace\\build\\test\\src\\Release\\XXXTest.exe")
+try:
+    ret = subprocess.check_call("C:\\workspace\\build\\test\\src\\Release\\XXXTest.exe")
+except subprocess.CalledProcessError as e:
+    print(e.returncode)
+    print(e.cmd)
+    print(e.output)
+except:
+    print("error")
